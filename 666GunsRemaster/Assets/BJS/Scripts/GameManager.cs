15f3fedf52f6e7b77f89d817fa599486e4f50b16
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2024-09-19 15:37:56 +0900

    [변지상] 기본적인 싱글턴 및 GameManager 세팅 테스트

--- 666GunsRemaster/Assets/BJS/Scripts/GameManager.cs
+++ 666GunsRemaster/Assets/BJS/Scripts/GameManager.cs
@@ -1,9 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
+
+    //싱글턴
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //게임 재시작
+    public void Restart()
+    {
+        SceneManager.LoadScene("Practice");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
