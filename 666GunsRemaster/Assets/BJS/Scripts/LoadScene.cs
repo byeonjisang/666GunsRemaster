7782f3b0b06f3c3a93ec973a4dc9c70dd7f232ec
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2024-10-01 18:06:55 +0900

    [변지상] 로딩 씬 구현 완료

--- 666GunsRemaster/Assets/BJS/Scripts/LoadScene.cs
+++ 666GunsRemaster/Assets/BJS/Scripts/LoadScene.cs
@@ -2,11 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
-    public void LoadGameScene()
+    static string nextScene;
+
+    [SerializeField] 
+    Image progressBar;
+    public static void LoadGameScene(string sceneName)
     {
-        SceneManager.LoadScene("Practice");
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+
+    void Start()
+    {
+        //内风凭 角青
+        StartCoroutine(LoadSceneProcess());
+    }
+
+    IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float time = 0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            //其捞农 肺爹 柳青
+            if (op.progress < 0.9f)
+            {
+                progressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                time += Time.unscaledDeltaTime;
+
+                progressBar.fillAmount = Mathf.Lerp(0.9f, 1f, time);
+
+                if (progressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
     }
 }
---
+++ 666GunsRemaster/Assets/BJS/Scripts/SceneTester.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI; // Button을 사용하기 위한 네임스페이스
+
+public class SceneTester : MonoBehaviour
+{
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+
+    [SerializeField]
+    private Button sceneChangeButton; // Button 연결
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Button의 OnClick 이벤트에 SceneChange 메서드를 연결
+        sceneChangeButton.onClick.AddListener(SceneChange);
+        canvasGroup.alpha = 1f; // alpha 값을 정확히 1로 설정
+    }
+
+    public void SceneChange()
+    {
+        StartCoroutine(FadeCanvasGroup());
+    }
+
+    IEnumerator FadeCanvasGroup()
+    {
+        float time = 0f;
+        float duration = 1f; // alpha 변화가 일어나는 시간
+
+        while (time <= duration)
+        {
+            time += Time.unscaledDeltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, time / duration); // 0에서 1까지 alpha 값 변화
+            yield return null; // 한 프레임 대기
+        }
+
+        LoadScene.LoadGameScene("Practice"); // alpha가 1이 되었을 때 씬 변경
+    }
+}
