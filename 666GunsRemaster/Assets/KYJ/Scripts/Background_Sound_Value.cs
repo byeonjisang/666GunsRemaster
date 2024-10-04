de81c87dcd137df7e50d41df60aaee82a2985ec7
KIMYOUNGJUN\kyj92 <kyj92727@gmail.com>
2024-10-04 17:22:31 +0900

    [김영준]로딩씬 구현

--- 666GunsRemaster/Assets/KYJ/Scripts/Background_Sound_Value.cs
+++ 666GunsRemaster/Assets/KYJ/Scripts/Background_Sound_Value.cs
@@ -8,7 +8,7 @@ public class Background_Sound_Value : MonoBehaviour
     public Slider volumeSlider;
     public Text volumeText;
 
-    private void Start()
+    void Start()
     {
         // 슬라이더의 값 변경 시 호출되는 함수 등록
         volumeSlider.onValueChanged.AddListener(UpdateVolumeText);
@@ -18,7 +18,7 @@ public class Background_Sound_Value : MonoBehaviour
     }
 
     // 슬라이더 값에 따라 텍스트 업데이트
-    private void UpdateVolumeText(float value)
+    void UpdateVolumeText(float value)
     {
         volumeText.text = (value * 100 ).ToString("F0"); // 퍼센트로 표시
         // 여기서 오디오 시스템에 값 적용 (예: AudioListener.volume = value;)
--- 666GunsRemaster/Assets/KYJ/Scripts/Effect_Sound_Setting_Value.cs
+++ 666GunsRemaster/Assets/KYJ/Scripts/Effect_Sound_Setting_Value.cs
@@ -8,7 +8,7 @@ public class Effect_Sound_Value : MonoBehaviour
     public Slider volumeSlider; // 슬라이더
     public Text volumeText;     // 볼륨 텍스트
 
-    private void Start()
+    void Start()
     {
         // 슬라이더의 값 변경 시 호출되는 함수 등록
         volumeSlider.onValueChanged.AddListener(UpdateVolumeText);
@@ -18,7 +18,7 @@ public class Effect_Sound_Value : MonoBehaviour
     }
 
     // 슬라이더 값에 따라 텍스트 업데이트
-    private void UpdateVolumeText(float value)
+     void UpdateVolumeText(float value)
     {
         volumeText.text = (value * 100).ToString("F0");
     }
