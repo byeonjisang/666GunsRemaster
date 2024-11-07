110e37752148dbaef4659625355eedd08ee0beed
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2024-11-07 16:24:06 +0900

    [변지상] 리소스 일부 추가 및 폰트 추가/맵 수정

---
+++ 666GunsRemaster/Assets/Main/Scripts/Position/ChangePosition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+    public List<GameObject> posList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
