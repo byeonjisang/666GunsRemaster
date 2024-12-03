81aaf0b671432d38c2e86486a584a7045fa3e6ee
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2024-12-03 13:16:21 +0900

    [변지상] 몬스터 관련 사운드 적용 완료

--- 666GunsRemaster/Assets/Main/Scripts/Enemy/Police1/Police1Stats.cs
+++ 666GunsRemaster/Assets/Main/Scripts/Enemy/Police1/Police1Stats.cs
@@ -82,8 +82,6 @@ public class Police1Stats : MonoBehaviour
         {
             if (hit.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
-                //소리 재생
-                SoundManager.instance.PlayEffectSound(9);
                 // 플레이어에게 데미지 전달
                 hit.GetComponent<PlayerController>().SetHp(police1.GetDamage());
                 Debug.Log("플레이어에게 근거리 공격을 가했습니다.");
@@ -92,6 +90,12 @@ public class Police1Stats : MonoBehaviour
                 break; // 첫 번째 플레이어에게만 데미지를 전달한 후 종료
             }
         }
+    }
+
+    public void PlayAttackSound()
+    {
+        //소리 재생
+        SoundManager.instance.PlayEffectSound(9);
     }
 
     private void DetectPlayer()
