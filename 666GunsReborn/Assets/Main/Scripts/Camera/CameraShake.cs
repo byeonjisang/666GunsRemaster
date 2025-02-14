c32e20b00ea8cff7ede4ea1cc0bf392b5495f250
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2025-02-14 18:23:10 +0900

    [변지상] 옵션창에 화면 흔들림기능 on/off 추가 중

--- 666GunsReborn/Assets/Main/Scripts/Camera/CameraShake.cs
+++ 666GunsReborn/Assets/Main/Scripts/Camera/CameraShake.cs
@@ -6,6 +6,7 @@ public class CameraShake : MonoBehaviour
 {
     public Transform shakeCamera;
     private bool isShakeRotate = false;
+    public bool shakeOption = true;
 
     //흔들릴 Transform 값
     public Vector3 originPos;
@@ -19,10 +20,16 @@ public class CameraShake : MonoBehaviour
 
     void Update()
     {
+        //테스트용(추후 옮겨질 예정)
         if(Input.GetKey(KeyCode.Z))
         {
             StartCoroutine(Shake());
         }
+    }
+
+    public void ShakeOptionSetting(bool value)
+    {
+        shakeOption = value;
     }
 
     public IEnumerator Shake(float dur = 0.01f, float magnitudePos = 0.03f, float magnitudeRot = 0.1f)
@@ -34,7 +41,7 @@ public class CameraShake : MonoBehaviour
             Vector3 shakePos = Random.insideUnitSphere;
             shakeCamera.localPosition = shakePos * magnitudePos;
 
-            if (isShakeRotate)
+            if (isShakeRotate && shakeOption)
             {
                 Vector3 shakeRot = new Vector3(0, 0,  Mathf.PerlinNoise(magnitudeRot, magnitudePos));
 
