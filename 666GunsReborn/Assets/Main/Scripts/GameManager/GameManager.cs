e548fbcb22fe22b6304fd84915479ab062c5c1ff
JISNAGGRAM\wltkd <pannasonic74@gmail.com>
2025-04-06 16:50:40 +0900

    [변지상] 포탈 기능을 통한 GameManager 모드 변경 구현 중

--- 666GunsReborn/Assets/Main/Scripts/GameManager/GameManager.cs
+++ 666GunsReborn/Assets/Main/Scripts/GameManager/GameManager.cs
@@ -1,25 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
+//전체 GameManager이기 떄문에 현재 어떤 상태인지 구분.
+public enum GameMode
+{
+    LOBBY,
+    GAMESTART,
+    INGAME
+}
 public class GameManager : MonoBehaviour
 {
-    //스테이지를 관리할 리스트
-    public List<int> stageList = new List<int>();
-
-    //재화를 보여줄 텍스트
-    public Text goldText;
-
     void Awake()
     {
         
     }
-
+    // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    // Update is called once per frame
     void Update()
     {
         
--- 666GunsReborn/Assets/Main/Scripts/Lobby/Portal.cs
+++ 666GunsReborn/Assets/Main/Scripts/Lobby/Portal.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         
     }
