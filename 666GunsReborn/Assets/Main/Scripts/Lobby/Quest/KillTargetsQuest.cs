0706e09cd09c893fd31b87fdc282c9903e5b9b69
jisnagbyeon <pannasonic74@gmail.com>
2025-06-22 23:08:00 +0900

    [변지상] 글자체 영어로 변경

--- 666GunsReborn/Assets/Main/Scripts/Lobby/Quest/KillTargetsQuest.cs
+++ 666GunsReborn/Assets/Main/Scripts/Lobby/Quest/KillTargetsQuest.cs
@@ -6,18 +6,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KillTargets_Quest", menuName = "Datas/KillTargets_Quest", order = 1)]
 public class KillTargets_Quest : QuestBase
 {
-    [Header("처치해야 할 적의 ID")]
     public int enemyId;
 
-    [Header("목표 처치 수")]
     public int targetKillCount;
 
-    [Header("현재까지 처치한 수")]
     public int currentKillCount;
 
     public override string GetFormatText()
     {
-        return $"적 처치: {currentKillCount}/{targetKillCount}";
+        return "Enemy Killed: {currentKillCount}/{targetKillCount}";
     }
 
     public void AddKill()
