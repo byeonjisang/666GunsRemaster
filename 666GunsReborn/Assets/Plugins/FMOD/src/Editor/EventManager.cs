9f089cfa30a0d20e9e5f2ca77ce0034c0f808fdb
jisnagbyeon <pannasonic74@gmail.com>
2025-08-01 14:03:29 +0900

    [변지상] FMOD 패키지 설치

--- 666GunsReborn/Assets/Plugins/FMOD/src/Editor/EventManager.cs
+++ 666GunsReborn/Assets/Plugins/FMOD/src/Editor/EventManager.cs
@@ -51,6 +51,7 @@ namespace FMODUnity
             eventCache.CacheTime = DateTime.MinValue;
             eventCache.EditorBanks.Clear();
             eventCache.EditorEvents.Clear();
+            eventCache.EditorEventsDict.Clear();
             eventCache.EditorParameters.Clear();
             eventCache.StringsBanks.Clear();
             eventCache.MasterBanks.Clear();
@@ -356,6 +357,7 @@ namespace FMODUnity
                 });
                 eventCache.EditorParameters.RemoveAll((x) => x == null);
 
+                eventCache.BuildDictionary();
                 AssetDatabase.SaveAssets();
             }
             finally
@@ -1224,7 +1226,13 @@ namespace FMODUnity
         public static EditorEventRef EventFromString(string path)
         {
             AffirmEventCache();
-            return eventCache.EditorEvents.Find((x) => x.Path.Equals(path, StringComparison.CurrentCultureIgnoreCase));
+
+            if (eventCache.EditorEventsDict.TryGetValue(path, out int index))
+            {
+                return eventCache.EditorEvents[index];
+            }
+
+            return null;
         }
 
         public static EditorEventRef EventFromGUID(FMOD.GUID guid)
