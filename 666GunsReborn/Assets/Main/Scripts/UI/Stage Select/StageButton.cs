6bcd9f3748bc3b076a3df8a449bc2e8a978dd55e
MinHyeo <p610222@naver.com>
2026-01-03 16:59:02 +0900

    [유민형] 이전 스테이지를 클리어하면 다음 스테이지 해금되게 수정

--- 666GunsReborn/Assets/Main/Scripts/UI/Stage Select/StageButton.cs
+++ 666GunsReborn/Assets/Main/Scripts/UI/Stage Select/StageButton.cs
@@ -13,12 +13,12 @@ public class StageButton : MonoBehaviour
 
 
     // 스테이지 데이터로 버튼 초기화
-    public void Init(StageData stageData)
+    public void Init(int stageIndex, bool isCleared)
     {
         // 스테이지 별 이름 설정
-        _stageNameText.text = "Stage " + stageData.stageIndex;
+        _stageNameText.text = "Stage " + stageIndex;
         // 스테이지 클리어 여부에 따라 잠금 이미지 활성화 설정 맟 버튼 상호작용 설정
-        _lockImage.SetActive(!stageData.isCleared);
-        _button.interactable = stageData.isCleared;
+        _lockImage.SetActive(!isCleared);
+        _button.interactable = isCleared;
     }
 }
--- 666GunsReborn/Assets/Main/Scripts/UI/Stage Select/View/StageSelectView.cs
+++ 666GunsReborn/Assets/Main/Scripts/UI/Stage Select/View/StageSelectView.cs
@@ -26,12 +26,18 @@ namespace StageSelect
         // 스테이지 버튼 생성 메서드
         private void CreateStageButtons(List<StageData> allStageData)
         {
-            foreach (var stageData in allStageData)
+            bool before_cleared = false;
+            for(int i = 0; i < allStageData.Count; i++)
             {
+                // allStageData[i]를 사용하면 i를 참조하여 ArrayIndexOutOfBounds 예외가 발생할 수 있으므로 별도의 변수에 할당
+                StageData stageData = allStageData[i];
+
                 // 버튼 생성 및 초기화
                 GameObject stageButtonObject = Instantiate(_stageButtonPrefab, _stageButtonParent);
                 StageButton stageButton = stageButtonObject.GetComponent<StageButton>();
-                stageButton.Init(stageData);
+                // 1스테이지는 무조건 오픈, 그 외에는 이전 스테이지 클리어 여부에 따라 설정
+                stageButton.Init(stageData.stageIndex, i == 0 ? true : before_cleared);
+                before_cleared = stageData.isCleared;
 
                 // 버튼 클릭 리스너 설정
                 Button buttonComponent = stageButtonObject.GetComponent<Button>();
